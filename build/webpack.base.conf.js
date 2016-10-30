6817c2c0501cf53686e47d2b42631316d2ff8129
yancey <yancey1204@gmail.com>
2016-10-30 14:46:57 +0800

    move src folder to app/client

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -12,7 +12,7 @@ var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './app/client/main.js'
   },
   output: {
     path: config.build.assetsRoot,
@@ -24,9 +24,9 @@ module.exports = {
     fallback: [path.join(__dirname, '../node_modules')],
     alias: {
       'vue$': 'vue/dist/vue',
-      'src': path.resolve(__dirname, '../src'),
-      'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components')
+      'src': path.resolve(__dirname, '../app/client'),
+      'assets': path.resolve(__dirname, '../app/client/assets'),
+      'components': path.resolve(__dirname, '../app/client/components')
     }
   },
   resolveLoader: {
