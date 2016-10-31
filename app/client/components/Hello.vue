9a937a16dea136708d917dc7ef7d6eab7099784c
Zhihui Yan <zhihui_yan@epam.com>
2016-10-31 18:37:33 +0800

    added one question and config webpack for server

--- app/client/components/Hello.vue
+++ app/client/components/Hello.vue
@@ -1,6 +1,10 @@
 <template lang="jade">
 div.hello
-  h1 {{ msg }}
+  span Shortcut for 
+  h1 {{ question }}
+  ul
+    li(v-for="option in options") 
+      button {{ option.text }}
 </template>
 
 <script>
@@ -8,7 +12,12 @@ export default {
   name: 'hello',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      question: 'Search everywhere',
+      options: [
+        { text: 'cmd + A' },
+        { text: 'double shift' },
+        { text: 'cmd + F' },
+      ],
     };
   },
 };
@@ -16,7 +25,27 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" scoped>
+fontSize = 16px
+
 h1
   font-weight: normal
+  display: inline-block
+
+ul
+  list-style: none
+  
+  li
+    display: inline-block
+    margin: 0 fontSize
+
+    button
+      background-color: #4CAF50;
+      border: none;
+      color: white;
+      padding: 10px 25px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: fontSize;
 
 </style>
--- app/server/index.js
+++ app/server/index.js
@@ -2,60 +2,87 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+const isProd = process.env.NODE_ENV === 'production';
+const PORT = isProd ? process.env.PORT : 8080;        // set our PORT
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddlwware = require('webpack-hot-middleware');
+const historyFallback = require('connect-history-api-fallback');
+const webpackConfig = isProd
+  ? require('../../build/webpack.prod.conf')
+  : require('../../build/webpack.dev.conf');
+
 const app = express();
-const port = process.env.PORT || 8080;        // set our port
+// const mongoose = require('mongoose');
 
-const mongoose = require('mongoose');
+if (!isProd) {
+  const compiler = webpack(webpackConfig);
+  const devMiddleware = webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats: {
+      colors: true,
+      chunks: false,
+    },
+  });
 
-mongoose.connect('mongodb://localhost/test'); // connect to our database
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('connected to mongodb');
-  // we're connected!
-});
+  const hotMiddleware = webpackHotMiddlwware(compiler);
+  // force page reload when html-webpack-plugin template changes
+  compiler.plugin('compilation', (compilation) => {
+    compilation.plugin('html-webpack-plugin-after-emit', (data, cb) => {
+      hotMiddleware.publish({ action: 'reload' });
+      cb();
+    });
+  });
 
-const Bear = require('./model/bear');
+  // handle fallback for HTML5 history API
+  app.use(historyFallback());
 
+  // serve webpack bundle output
+  app.use(devMiddleware);
+
+  // enable hot-reload and state-preserving
+  // compilation error display
+  app.use(hotMiddleware);
+}
 
 app.use(express.static('dist'));
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-// ROUTES FOR OUR API
-// =============================================================================
-const router = express.Router();              // get an instance of the express Router
+// // ROUTES FOR OUR API
+// // =============================================================================
+// const router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', (req, res) => {
-  res.send(path.resolve(__dirname, '../../dist/index.html'));   
-});
+// // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// router.get('/', (req, res) => {
+//   res.send(path.resolve(__dirname, '../../dist/index.html'));   
+// });
 
-router.route('/bears')
-  // create a bear (accessed at POST http://localhost:8080/api/bears)
-  .post((req, res) => {
-    let bear = new Bear();      // create a new instance of the Bear model
-    bear.name = req.body.name;  // set the bears name (comes from the request)
+// router.route('/bears')
+//   // create a bear (accessed at POST http://localhost:8080/api/bears)
+//   .post((req, res) => {
+//     let bear = new Bear();      // create a new instance of the Bear model
+//     bear.name = req.body.name;  // set the bears name (comes from the request)
 
-    // save the bear and check for errors
-    bear.save(function(err) {
-      if (err)
-        res.send(err);
+//     // save the bear and check for errors
+//     bear.save(function(err) {
+//       if (err)
+//         res.send(err);
 
-        res.json({ message: 'Bear created!' });
-      });
+//         res.json({ message: 'Bear created!' });
+//       });
       
-});
+// });
 
-// more routes for our API will happen here
+// // more routes for our API will happen here
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+// // REGISTER OUR ROUTES -------------------------------
+// // all of our routes will be prefixed with /api
+// app.use('/api', router);
 
-app.listen(port, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
